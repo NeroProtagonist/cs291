fefe945cfdb2ad1d4d30900611bc614720e2ad7b
NeroProtagonist <NeroProtagonist@users.noreply.github.com>
2019-06-16 22:09:45 +0200

    Add unit 4 solutions.

--- unit4/unit4-flowersquish_exercise.js
+++ unit4/unit4-flowersquish_exercise.js
@@ -43,12 +43,18 @@ function fillScene() {
 	// Scales, rotates, and positions on the cylinder and petals are needed.
 	// The petals should be squished and be 1/4 as thick as wide
 	// and they should be tilted 20 degrees up from the position in the previous exercise
-
-	var cylinder = new THREE.Mesh( cylGeom, petalMaterial );
-	var petal = new THREE.Object3D();
-	petal.add( cylinder );
-
-	flower.add( petal );
+	var numPetals = 24;
+	for ( var i = 0; i < numPetals; ++i ) {
+		var cylinder = new THREE.Mesh( cylGeom, petalMaterial );
+		cylinder.position.y = petalLength / 2;
+		cylinder.scale.x = 0.25;
+		var petal = new THREE.Object3D();
+		petal.position.y = flowerHeight;
+		petal.rotation.z = ( 90 - 20 ) * Math.PI / 180;
+		petal.rotation.y = 2 * Math.PI * i / numPetals;
+		petal.add( cylinder );
+		flower.add( petal );
+	}
 
 	// Rest of the flower
 	var stamenMaterial = new THREE.MeshLambertMaterial( { color: 0x333310 } );
